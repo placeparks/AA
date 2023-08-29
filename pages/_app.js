02c9c2b171b6d81f996e9c9cfaab2be510e6f31f
placeparks <placeparks@gmail.com>
2023-08-29 14:17:26 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import '../styles/globals.css';
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = 'ethereum';
+const activeChain = 'mumbai';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThirdwebProvider
 			activeChain={activeChain}
-			clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+			clientId="b348d6571045948f6cc34cffc3589041"
 		>
 			<Component {...pageProps} />
 		</ThirdwebProvider>
